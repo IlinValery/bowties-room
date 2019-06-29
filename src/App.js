44b1f8b631e2f97350985c00debe78711ba8d7f8
Valery Ilin <v.s.ilin@ya.ru>
2019-06-29 18:56:07 +0300

    fix items

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import Col from "reactstrap/es/Col";
 import Button from "reactstrap/es/Button";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Item from "./Components/Item";
 library.add(faShoppingCart);
 
 const image0 = require('./images/item0.jpg');
 const image1 = require('./images/item1.jpg');
 const image2 = require('./images/item2.jpg');
+const image3 = require('./images/item3.jpg');
+const image4 = require('./images/item4.jpg');
+const image5 = require('./images/item5.jpg');
 
 const team_img = require('./images/team.JPG');
 
@@ -23,11 +25,12 @@ export default class App extends React.Component {
 
     this.state={
         items: [
-            {id:2, name: "A Science Bow Tie", image:image2, description:"Top bow tie :)"},
-            {id:0, name: "A Math Bow Tie (Stokes' theorem)", image:image0, description:"An elegant bow tie for a smart guy."},
-            {id:3, name: "A Steam Punk Bow Tie", image:image1, description:"An super elegant bow tie :)"},
-            {id:1, name: "A Math Bow Tie (Hopf fibration)", image:image1, description:"An elegant bow tie for a smart guy."},
-
+            {id:0, name: "A Science Bow Tie", image:image2, description:"Top bow tie :)"},
+            {id:1, name: "A Steam Punk Bow Tie", image:image3, description:"An super elegant bow tie :)"},
+            {id:2, name: "Iron Man Bow Tie", image:image4, description:"Super limited bow tie"},
+            {id:3, name: "A Math Bow Tie (Stokes' theorem)", image:image0, description:"An elegant bow tie for a smart guy."},
+            {id:4, name: "A Math Bow Tie (Hopf fibration)", image:image1, description:"An elegant bow tie for a smart guy."},
+            {id:5, name: "A Math Bow Tie (Maxwell's equations)", image:image5, description:"Last bow tie in our collection"}
         ]
     }
 
@@ -54,14 +57,15 @@ export default class App extends React.Component {
                 <h1 className={"text-center"} style={{marginTop: "8px",marginBottom: "32px", color: "white" }}><b>Sciencishbowties showroom</b></h1>
                 <Row>
                     <Col/>
-                    <Col xl={"6"} sm={"12"}>
+                    <Col xl={"7"} sm={"12"}>
                         <ExpTimer/>
                     </Col>
                     <Col/>
                 </Row>
+                <h2 className={"text-center"} style={{marginTop: "8px",marginBottom: "10px", color: "white" }}><b>Our products</b></h2>
                 <Row>
                     <Col/>
-                    <Col xl={"10"} xs={"12"}>
+                    <Col xl={"9"} xs={"10"}>
                         <Row>
                             {this.renderElemets(this.state.items)}
                         </Row>
@@ -73,7 +77,7 @@ export default class App extends React.Component {
                 <Row>
                     <Col/>
                     <Col className={"text-center"} sm={"12"} md={5}>
-                        <img className={"img-fluid"} src={String(team_img)} alt={"Our team photo"}/>
+                        <img className={"img-fluid"} src={String(team_img)} alt={"Our team"}/>
                     </Col>
                     <Col md={4}>
                         <ul style={{color: "white", fontSize: "18pt"}}>
