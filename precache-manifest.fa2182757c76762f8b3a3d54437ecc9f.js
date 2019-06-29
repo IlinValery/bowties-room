bf526cf42701bff0afe5024223ca9975c81dbe27
Valery Ilin <v.s.ilin@ya.ru>
2019-06-29 09:17:28 +0300

    Updates

---
+++ precache-manifest.fa2182757c76762f8b3a3d54437ecc9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23f533c84e6c6652ba43057c096221e",
+    "url": "/showroom/index.html"
+  },
+  {
+    "revision": "d024e8a5d1f9adc880a2",
+    "url": "/showroom/static/css/main.6942973f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/showroom/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d024e8a5d1f9adc880a2",
+    "url": "/showroom/static/js/main.3c3e8a28.chunk.js"
+  },
+  {
+    "revision": "0505c27aae92cf8cc990",
+    "url": "/showroom/static/js/runtime~main.81125bbf.js"
+  }
+]);
